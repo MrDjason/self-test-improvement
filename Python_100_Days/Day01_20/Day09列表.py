19a1dedf4774aec7866133366fc61816669f9618
MrDjason <13218798893@163.com>
2025-09-08 23:07:31 +0800

    学习

--- Python_100_Days/Day01_20/Day09列表.py
+++ Python_100_Days/Day01_20/Day09列表.py
@@ -30,3 +30,52 @@ items.sort()
 print(items) # sort从小到大进行排序如123，abc
 items.reverse()
 print(items) # 倒转
+
+# 列表生成式
+# 例一
+items = []
+for i in range(1,101):
+    if i % 3 == 0 or i % 5 ==0:
+        items.append(i)
+print(items)
+
+num_list = [i for i in range(1,101) if i % 3 == 0 or i % 5 == 0]
+print(num_list)
+
+# 例二
+num_list_1 = [35, 12, 97, 64, 55]
+num_list_2 = []
+for num in num_list_1:
+    num_list_2.append(num**2)
+print(num_list_2)
+
+num_list_3 = [num**2 for num in num_list_1]
+print(num_list_3)
+
+# 例三
+num_list_4 = []
+for num in num_list_1:
+    if num >50:
+        num_list_4.append(num)
+print(num_list_4)
+
+num_list_5 = [num for num in num_list_1 if num > 50]
+print(num_list_5)
+
+# 嵌套列表
+scores = [[100,99,98], [61,60,59], [0,1,2]]
+print(scores[2][1])
+
+# 嵌套列表 列表生成式
+import random
+
+scores = []
+for _ in range(5):
+    temp = []
+    for _ in range(3):
+        temp.append(random.randrange(60, 101))
+    scores.append(temp)
+print(scores)
+
+scores = [[random.randrange(60,101) for _ in range(3)] for _ in range(5)]
+print(scores)
