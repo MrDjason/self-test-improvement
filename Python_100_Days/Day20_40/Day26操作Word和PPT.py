ec0b75caf18c3688ddf6536372b7d3c0678bdb54
MrDjason <13218798893@163.com>
2025-10-15 09:41:02 +0800

    学习

--- Python_100_Days/Day20_40/Day26操作Word和PPT.py
+++ Python_100_Days/Day20_40/Day26操作Word和PPT.py
@@ -84,9 +84,11 @@ document.add_page_break()
 document.save(file_path + '/demo.docx')
 # 保存文档
 
-from docx import Document
-from docx.document import Document as Doc
 
+
+
+
+# 读取文档信息
 doc = Document(file_path + '/离职证明.docx')
 for no, p in enumerate(doc.paragraphs):
     print(no, p.text)
@@ -95,3 +97,48 @@ for no, p in enumerate(doc.paragraphs):
 
 # 我们可以把上面的离职证明制作成一个模板文件，把姓名、身份证号、入职和离职日期等信息用占位符代替
 # 这样通过对占位符的替换，就可以根据实际需要写入对应的信息
+
+employees = [
+    {
+        'name': '骆昊',
+        'id': '100200198011280001',
+        'sdate': '2008年3月1日',
+        'edate': '2012年2月29日',
+        'department': '产品研发',
+        'position': '架构师',
+        'company': '成都华为技术有限公司'
+    },
+    {
+        'name': '王大锤',
+        'id': '510210199012125566',
+        'sdate': '2019年1月1日',
+        'edate': '2021年4月30日',
+        'department': '产品研发',
+        'position': 'Python开发工程师',
+        'company': '成都谷道科技有限公司'
+    },
+    {
+        'name': '李元芳',
+        'id': '2102101995103221599',
+        'sdate': '2020年5月10日',
+        'edate': '2021年3月5日',
+        'department': '产品研发',
+        'position': 'Java开发工程师',
+        'company': '同城企业管理集团有限公司'
+    },
+]
+
+for emp_dict in employees:
+    doc = Document(file_path + '/离职证明模板.docx')
+    for p in doc.paragraphs:
+        if '{' not in p.text:
+            continue
+        for run in p.runs:
+            if '{' not in run.text:
+                continue
+
+            start, end = run.text.find('{'), run.text.find('}')
+            key, place_holder = run.text[start+1:end], run.text[start:end+1]
+            run.text = run.text.replace(place_holder, emp_dict[key])
+
+    doc.save(f'{emp_dict["name"]}离职证明.docx')
